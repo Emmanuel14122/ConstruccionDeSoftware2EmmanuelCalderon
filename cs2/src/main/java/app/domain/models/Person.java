ad99245e2ba7601f4767e2e75440192787e36618
Emmanuel Calderon <emmanuel12251>
2026-03-03 16:02:21 -0500

    commit

---
+++ cs2/src/main/java/app/domain/models/Person.java
@@ -0,0 +1,26 @@
+package app.domain.models;
+
+import java.sql.Date;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public abstract class Person {
+
+    private int Id_User;
+    private String Id_Related;
+    private String Full_Name;
+    private String Id_Identification;
+    private String Email;
+    private String Phone;
+    private Date Date_Birth;
+    private String Address;
+    private String System_Role;
+    private String User_Status;
+
+}
---
+++ cs2/src/main/java/app/domain/models/Role.java
@@ -0,0 +1,11 @@
+package app.domain.models;
+
+public enum Role {
+    Natural_Person_Client,
+    Company_Client,
+    Counter_Employee,
+    Commercial_Employee,
+    Company_Employee,
+    Company_Supervisor,
+    Internal_Bank_Analyst,
+}
